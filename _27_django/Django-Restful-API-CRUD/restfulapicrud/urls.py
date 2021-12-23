1135182eb5ba97025b86ba5af773c6da57a4e876
MCS-0044-ali <93029574+alihussainmcs@users.noreply.github.com>
2021-12-23 17:12:57 +0530

    MCS Core Python

--- _27_django/Django-Restful-API-CRUD/restfulapicrud/urls.py
+++ _27_django/Django-Restful-API-CRUD/restfulapicrud/urls.py
@@ -1,4 +1,4 @@
-"""employee_project URL Configuration
+"""restfulapicrud URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('employee/', include('employee_register.urls'))
+    path('api/',include(router.urls))
 ]
--- _27_django/djangoTutorial/Hello/Hello/urls.py
+++ _27_django/djangoTutorial/Hello/Hello/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Harry Ice Cream Admin"
-admin.site.site_title = "Harry Ice Cream Admin Portal"
-admin.site.index_title = "Welcome to Harry Ice Creams"
+admin.site.site_header = "Ali Ice Cream Admin"
+admin.site.site_title = "Ali Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Ali Ice Creams"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
