1bd563583d2d6bd6cbd6698dd6213c96d981ae79
MCS-0044-ali <93029574+alihussainmcs@users.noreply.github.com>
2021-12-24 17:11:36 +0530

    MCS Core Python

---
+++ _28_restapi/zinggrid-django-rest-framework-crud/crudexample/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+# define the definition/function to return our template page
+def zinggrid(request):
+    title = "My ZingGrid CRUD Example(s)"
+    return render(request, 'zinggrid.html', {'title': title})
---
+++ _28_restapi/zinggrid-django-rest-framework-crud/myapi/serializers.py
@@ -0,0 +1,18 @@
+# serializers.py
+from rest_framework import serializers
+
+from .models import Hero, Todo
+
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        # id is auto generated primary key
+        fields = ('id', 'name', 'alias', 'age', 'email')
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        # id is auto generated primary key
+        fields = ('id', 'item')
